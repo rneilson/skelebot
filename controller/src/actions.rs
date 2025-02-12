2ec510ae811209b980476f9d8e26a642dbc89a55
Raymond Neilson <rneilson@interloper.ca>
2025-02-11 20:50:51 -0400

    Controller: add main-loop handling for keypresses and stick position updates

--- controller/src/actions.rs
+++ controller/src/actions.rs
@@ -1,21 +1,79 @@
+#![allow(dead_code)]
+
 use std::sync::mpsc::Sender;
 use std::time::Instant;
 
+use crossterm::event::KeyEvent;
+
+#[derive(Debug)]
+pub struct ControlState {
+    pub throttle: i16,
+    pub steering: i16,
+}
+
+impl ControlState {
+    pub fn as_u32(&self) -> u32 {
+        let high = self.throttle as u32;
+        let low = self.steering as u32;
+        (high << 16) | low
+    }
+
+    pub fn trim(&mut self) {
+        if self.throttle == i16::MIN {
+            self.throttle += 1;
+        }
+        if self.steering == i16::MIN {
+            self.steering += 1;
+        }
+    }
+}
+
+impl From<u32> for ControlState {
+    fn from(value: u32) -> Self {
+        Self {
+            throttle: (value >> 16) as i16,
+            steering: (value & 0xffff) as i16,
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct ThreadMsg {
     pub name: String,
     pub message: String,
 }
 
+#[derive(Debug)]
+pub struct StickPosition {
+    pub x: i16,
+    pub y: i16,
+}
+
+#[derive(Debug)]
 pub enum Action {
     Message(ThreadMsg),
     Error(ThreadMsg),
     Fatal(ThreadMsg),
+    KeyPress(KeyEvent),
+    StickUpdate(StickPosition),
 }
 
-pub fn record_ticks_for_period(tx: &Sender<Action>, name: &str, ticks: u32, prev_time: Instant, curr_time: Instant) {
+pub fn record_ticks_for_period(
+    tx: &Sender<Action>,
+    name: &str,
+    ticks: u32,
+    prev_time: Instant,
+    curr_time: Instant,
+) {
     // TODO: probably need to handle this more gracefully
-    let ms_since = curr_time.checked_duration_since(prev_time).unwrap().as_millis();
+    let ms_since = curr_time
+        .checked_duration_since(prev_time)
+        .unwrap()
+        .as_millis();
     let msg = format!("looped {0} times in {1}ms", ticks, ms_since);
-    let msg = ThreadMsg { name: name.to_owned(), message: msg };
+    let msg = ThreadMsg {
+        name: name.to_owned(),
+        message: msg,
+    };
     tx.send(Action::Message(msg)).unwrap();
 }
--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,7 +1,9 @@
-use std::thread;
-use std::sync::mpsc;
 use std::sync::atomic::{AtomicBool, AtomicU32, Ordering};
+use std::sync::mpsc;
+use std::thread;
 use std::time::{Duration, Instant};
+
+use crossterm::event::{KeyCode, KeyEvent};
 
 mod actions;
 mod joystick;
@@ -9,7 +11,7 @@ mod radio;
 mod term;
 mod ui;
 
-use actions::Action;
+use actions::{Action, ControlState, StickPosition};
 
 fn main() {
     let exit_flag = AtomicBool::new(false);
@@ -22,16 +24,24 @@ fn main() {
     let u_tx = tx.clone();
     drop(tx);
 
-    // let control_state = AtomicU32::new(0);
+    let control_state_atomic = AtomicU32::new(0);
 
     println!("Starting up...");
     let exit_at = Instant::now() + Duration::from_secs(5);
 
     thread::scope(|s| {
-        s.spawn(|| {joystick::collect_joystick_events(j_tx, &exit_flag);});
-        s.spawn(|| {radio::radio_comms(r_tx, &exit_flag);});
-        s.spawn(|| {term::collect_terminal_events(t_tx, &exit_flag);});
-        s.spawn(|| {ui::draw_ui(u_tx, &exit_flag);});
+        s.spawn(|| {
+            joystick::collect_joystick_events(j_tx, &exit_flag);
+        });
+        s.spawn(|| {
+            radio::radio_comms(r_tx, &exit_flag);
+        });
+        s.spawn(|| {
+            term::collect_terminal_events(t_tx, &exit_flag);
+        });
+        s.spawn(|| {
+            ui::draw_ui(u_tx, &exit_flag);
+        });
 
         // Loop over channel rx and process events
         // TEMP: until quittin' time
@@ -42,26 +52,48 @@ fn main() {
                     match action {
                         Action::Message(msg) => {
                             println!("{0}: {1}", msg.name, msg.message);
-                        },
+                        }
                         Action::Error(err) => {
                             eprintln!("Error from {0}: {1}", err.name, err.message);
-                        },
+                        }
                         Action::Fatal(err) => {
-                            err_msg = Some(format!("Fatal error from {0}: {1}", err.name, err.message));
+                            err_msg =
+                                Some(format!("Fatal error from {0}: {1}", err.name, err.message));
                             exit_flag.store(true, Ordering::Relaxed);
-                        },
+                        }
+                        Action::KeyPress(key_event) => {
+                            let control_state =
+                                ControlState::from(control_state_atomic.load(Ordering::Relaxed));
+                            match handle_keypress_event(control_state, key_event) {
+                                Some(control_state) => {
+                                    control_state_atomic
+                                        .store(control_state.as_u32(), Ordering::Relaxed);
+                                    // TODO: send update message to UI thread
+                                    println!("Control state: {:?}", control_state);
+                                }
+                                None => {
+                                    exit_flag.store(true, Ordering::Relaxed);
+                                }
+                            }
+                        }
+                        Action::StickUpdate(stick_pos) => {
+                            let control_state = handle_stick_position(stick_pos);
+                            control_state_atomic.store(control_state.as_u32(), Ordering::Relaxed);
+                            // TODO: send update message to UI thread
+                            println!("Control state: {:?}", control_state);
+                        }
                     }
-                },
+                }
                 Err(mpsc::RecvTimeoutError::Timeout) => {
                     // If we've timed out after signalling exit, just break
                     if exit_flag.load(Ordering::Relaxed) {
                         break 'listener;
                     }
-                },
+                }
                 Err(mpsc::RecvTimeoutError::Disconnected) => {
                     // Disconnected implies all senders dropped
                     break 'listener;
-                },
+                }
             }
             if Instant::now() >= exit_at {
                 exit_flag.store(true, Ordering::Relaxed);
@@ -74,4 +106,53 @@ fn main() {
     }
 
     println!("Shutting down...");
+}
+
+/// Returns a modified control state if arrow keys are pressed, or None if the quit
+/// key ('q' at present) is pressed
+fn handle_keypress_event(
+    mut control_state: ControlState,
+    key_event: KeyEvent,
+) -> Option<ControlState> {
+    match key_event.code {
+        // Quit on 'q'
+        KeyCode::Char('q') => {
+            return None;
+        }
+        // Manipulate control state on arrow keys
+        KeyCode::Up => {
+            control_state.throttle = control_state.throttle.saturating_add(8_192);
+        }
+        KeyCode::Down => {
+            control_state.throttle = control_state.throttle.saturating_sub(8_192);
+        }
+        KeyCode::Right => {
+            control_state.steering = control_state.steering.saturating_add(8_192);
+        }
+        KeyCode::Left => {
+            control_state.steering = control_state.steering.saturating_sub(8_192);
+        }
+        // Reset control state to center on spacebar
+        KeyCode::Char(' ') => {
+            control_state.throttle = 0;
+            control_state.steering = 0;
+        }
+        // Ignore others
+        _ => {}
+    }
+    control_state.trim();
+    Some(control_state)
+}
+
+/// Converts a joystick position to a new control state
+fn handle_stick_position(stick_pos: StickPosition) -> ControlState {
+    // Convert stick position to control state
+    // At present this is a simple mapping of Y axis to throttle
+    // and X axis to steering
+    let mut control_state = ControlState {
+        throttle: stick_pos.y,
+        steering: stick_pos.x,
+    };
+    control_state.trim();
+    control_state
 }
