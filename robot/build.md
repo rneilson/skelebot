9827e284a1f3e21adae13b87d0e733846ee66cd3
Raymond Neilson <rneilson@interloper.ca>
2025-03-31 12:55:02 -0300

    Robot: turn placeholder code into actual-robot code, et voila!

--- robot/build.md
+++ robot/build.md
@@ -8,3 +8,8 @@ From `robot/` subdir:
 arduino-cli compile -b arduino:avr:pro --board-options cpu=8MHzatmega328 ./placeholder/
 arduino-cli upload -p /dev/ttyUSB0 -b arduino:avr:pro --board-options cpu=8MHzatmega328 ./placeholder/
 ```
+
+```
+arduino-cli compile -b arduino:avr:pro --board-options cpu=16MHzatmega328 --libraries ./vendor/ ./herakles/
+arduino-cli upload -p /dev/ttyUSB0 -b arduino:avr:pro --board-options cpu=16MHzatmega328 ./herakles/
+```
--- robot/herakles/herakles.ino
+++ robot/herakles/herakles.ino
@@ -1,9 +1,141 @@
 #include <Arduino.h>
 #include <RF24.h>
-#include "motordriver_4wd.h"
+#include <motordriver_4wd.h>
+
+#define CE_PIN A0
+#define CSN_PIN A1
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+// Command buffer
+uint8_t commands[4][4];
+uint8_t command_head = 0;
+uint8_t command_tail = 0;
+
+// Radio params
+uint8_t channel = 76;   // Default for RF24 lib, Crazyradio needs changing
+uint8_t control_addr[6] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7, 0x00}; // Default for Crazyradio
+
+// Timer values
+#define ACK_MS 50
+unsigned long next_ack = 0;
+
+// // Motor values
+// char motor_dir = 'S';
+// int8_t motor_left = 0;
+// int8_t motor_right = 0;
+
 
 void setup() {
+    MOTOR.init();
+
+    // Radio init
+    if (!radio.begin()) {
+        while (1) {}
+    }
+    // Set PA level low for debug
+    radio.setPALevel(RF24_PA_LOW);
+    radio.setDataRate(RF24_250KBPS);
+    // Enable dynamic payloads and payload acks
+    radio.enableDynamicPayloads();
+    radio.enableAckPayload();
+    // Set channel and address, reading from crazyradio tx
+    radio.openReadingPipe(0, control_addr);
+    radio.setChannel(channel);
+    // Put radio in RX mode
+    radio.startListening();
 }
 
 void loop() {
+    // First, handle any payload we've received
+    // Only one payload received per loop for now, may refactor later
+    if (radio.available()) {
+        uint8_t command_len = radio.getDynamicPayloadSize();
+        // Really should have an error flag somewhere around here
+        // We shouldn't ever get more than 4 bytes as per protocol
+        if (command_len > 4) {
+            command_len = 4;
+        }
+        uint8_t next_head = (command_head + 1) % 4;
+        // We have to drop payloads if the ring buffer's full
+        if (next_head != command_tail) {
+            radio.read(commands[next_head], command_len);
+            command_head = next_head;
+        }
+    }
+
+    // Potentially process multiple payloads, however
+    while (command_tail != command_head) {
+        // TODO: move this all to a function
+        uint8_t (&command)[4] = commands[command_tail];
+        switch (command[0]) {
+            case 0xF0:
+                // No-op
+                break;
+            case 0xF1:
+                // Change channel
+                radio.setChannel(command[1]);
+                break;
+            case 0xF2:
+                // (Reserved)
+                break;
+            case 0xF3:
+                // Stop
+                MOTOR.setStop1();
+                MOTOR.setStop2();
+                break;
+            case 0xF4:
+                // Forward (L+, R+)
+                MOTOR.setSpeedDir1(motorSpeedCeiling(command[1]), DIRF);
+                MOTOR.setSpeedDir2(motorSpeedCeiling(command[2]), DIRR);    // Right reversed
+                break;
+                case 0xF5:
+                // Turn right (L+, R-)
+                MOTOR.setSpeedDir1(motorSpeedCeiling(command[1]), DIRF);
+                MOTOR.setSpeedDir2(motorSpeedCeiling(command[2]), DIRF);    // Right reversed
+                break;
+            case 0xF6:
+                // Turn left (L-, R+)
+                MOTOR.setSpeedDir1(motorSpeedCeiling(command[1]), DIRR);
+                MOTOR.setSpeedDir2(motorSpeedCeiling(command[2]), DIRR);    // Right reversed
+                break;
+            case 0xF7:
+                // Backward (L-, R-)
+                MOTOR.setSpeedDir1(motorSpeedCeiling(command[1]), DIRR);
+                MOTOR.setSpeedDir2(motorSpeedCeiling(command[2]), DIRF);    // Right reversed
+                break;
+            case 0xF8:
+                // (Reserved)
+                break;
+            case 0xF9:
+                // (Reserved)
+                break;
+            default:
+                break;
+        }
+        command_tail = (command_tail + 1) % 4;
+    }
+
+    unsigned long current_tick = millis();
+
+    if (current_tick >= next_ack) {
+        // Update next ack and stage this one for sending
+        next_ack = current_tick + ACK_MS;
+
+        // Get divided battery voltage from ADC6
+        uint16_t voltage = analogRead(A6);
+        uint8_t *voltage_bytes = (uint8_t*)&voltage;
+
+        uint8_t ack[3];
+        ack[0] = 0xFB;  // Battery voltage
+        // Convert little-endian to big-endian
+        ack[1] = voltage_bytes[1];
+        ack[2] = voltage_bytes[0];
+
+        radio.writeAckPayload(0, ack, 3);
+    }
+}
+
+uint8_t motorSpeedCeiling(uint8_t value) {
+    return (uint8_t)(value < 100 ? value : 100);
 }
